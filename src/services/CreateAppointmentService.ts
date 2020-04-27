df6bca4197a3f4c2f7a7732e67effe8fbe065c88
TalesHabib <tales.habib@gmail.com>
2020-04-27 18:53:39 -0300

    trabalhando com banco de dados

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -10,27 +10,27 @@ interface Request {
 
 class CreateAppointmentService {
   private appointmentsRepository: AppointmentsRepository;
-  constructor(appointmentsRepository: AppointmentsRepository){
+  constructor(appointmentsRepository: AppointmentsRepository) {
     this.appointmentsRepository = appointmentsRepository;
   }
 
   public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
- const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
-   appointmentDate,
- )
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    )
 
- if(findAppointmentInSameDate) {
-   throw Error('This appointment is already booked');
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
 
- }
+    }
 
- const appointment = this.appointmentsRepository.create({
-    provider,
-    date: appointmentDate,
-   });
-   return appointment;
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+    return appointment;
   }
 }
 
